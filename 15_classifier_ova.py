d8a2678739928c0d797123132a9f1a07148045fe
Deepti Rawat <deeptirawat.iimt@gmail.com>
2023-10-31 22:20:47 +0530

    added code for OvA and OvO training and testing for SVM

---
+++ 15_classifier_ova.py
@@ -0,0 +1,66 @@
+# Import libraries
+import pandas as pd       # Importing for panel data analysis
+import pickle             # Loading model
+
+''' Let's analyze the data in **penguins_test.csv** '''
+# Import training data into pandas dataframe
+test_csv = pd.read_csv('./penguins_test.csv')
+# print('Shape of the dataset:', test_csv.shape)
+# test_csv.head(3)
+
+# Let's drop the null values, as to make predictions using the trained SVM ono-vs-all and one-vs-one classifiers, we need the values of all the attributes.
+test_csv.dropna(inplace=True)
+
+X_encoded = test_csv.copy()
+
+''' Let's normalize the data (continuous attributes). We are choosing z-score normalization to retain the outlier information. '''
+X_encoded["Culmen Length (mm)"] = (X_encoded["Culmen Length (mm)"] - X_encoded["Culmen Length (mm)"].mean())/X_encoded["Culmen Length (mm)"].std()
+X_encoded["Culmen Depth (mm)"] = (X_encoded["Culmen Depth (mm)"] - X_encoded["Culmen Depth (mm)"].mean())/X_encoded["Culmen Depth (mm)"].std()
+X_encoded["Flipper Length (mm)"] = (X_encoded["Flipper Length (mm)"] - X_encoded["Flipper Length (mm)"].mean())/X_encoded["Flipper Length (mm)"].std()
+X_encoded["Body Mass (g)"] = (X_encoded["Body Mass (g)"] - X_encoded["Body Mass (g)"].mean())/X_encoded["Body Mass (g)"].std()
+X_encoded["Delta 15 N (o/oo)"] = (X_encoded["Delta 15 N (o/oo)"] - X_encoded["Delta 15 N (o/oo)"].mean())/X_encoded["Delta 15 N (o/oo)"].std()
+X_encoded["Delta 13 C (o/oo)"] = (X_encoded["Delta 13 C (o/oo)"] - X_encoded["Delta 13 C (o/oo)"].mean())/X_encoded["Delta 13 C (o/oo)"].std()
+
+# test_csv.head(3)
+
+''' Let's preprocess this **test_csv** the same way as we did for train_csv '''
+# Prepare data. This format must match the `X_train_ova` or `X_train_ovo` from `15_Assignment4.ipynb`
+data = X_encoded[['Island', 'Clutch Completion', 'Culmen Length (mm)', 'Culmen Depth (mm)', 'Flipper Length (mm)', 'Body Mass (g)', 'Sex', 'Delta 15 N (o/oo)', 'Delta 13 C (o/oo)']]
+X_encoded = pd.get_dummies(data, columns=['Island'])
+X_encoded.replace({'Yes': 1, 'No': 0}, inplace=True)
+X_encoded.replace({'FEMALE': 1, 'MALE': 0}, inplace=True)
+X_encoded.replace({True: 1, False: 0}, inplace=True)
+
+svm_classifiers_ova = {} # Initialize a dictionary to store classifiers for each of the three binary class problems
+
+# load the model from disk
+loaded_model_adelie = pickle.load(open('./ova_svm_final_adelie.sav', 'rb'))
+svm_classifiers_ova['Adelie Penguin (Pygoscelis adeliae)'] = loaded_model_adelie
+loaded_model_gentoo = pickle.load(open('./ova_svm_final_gentoo.sav', 'rb'))
+svm_classifiers_ova['Gentoo penguin (Pygoscelis papua)'] = loaded_model_gentoo
+loaded_model_chinstrap = pickle.load(open('./ova_svm_final_chinstrap.sav', 'rb'))
+svm_classifiers_ova['Chinstrap penguin (Pygoscelis antarctica)'] = loaded_model_chinstrap
+# print(svm_classifiers_ova)
+# it should be of the form:
+# svm_classifiers_ova {'Adelie Penguin (Pygoscelis adeliae)': SVC(C=0.006, kernel='linear', random_state=42), 'Gentoo penguin (Pygoscelis papua)': SVC(C=0.006, kernel='linear', random_state=42), 'Chinstrap penguin (Pygoscelis antarctica)': SVC(C=0.006, kernel='linear', random_state=42)}
+
+''' Making predictions on Test set via trained binary classifiers '''
+per_class_pred_ova = {} # Initialize a dictionary to store prediction class
+
+for class_, svm_classifier in svm_classifiers_ova.items():
+    # Predict using the trained SVM classifier
+    y_pred_ova = svm_classifier.predict(X_encoded)
+    # print(f"For binary classifier with class - {class_}, \nthe predicted output for each input tuple is: {y_pred_ova}\n")
+    per_class_pred_ova[class_] = y_pred_ova
+
+# print("The final dictionary comprising the predicitions on input tuples from each of the three binary classifiers look like:")
+# print(per_class_pred_ova)
+
+# Combine the predictions to get multi-class predictions
+y_pred_multiclass_ova = pd.DataFrame(per_class_pred_ova).idxmax(axis=1)
+print(y_pred_multiclass_ova)
+
+''' Save the data to a csv file called `ova.csv`'''
+# Appending the prediction results in the dataframe
+test_csv['predicted'] = y_pred_multiclass_ova.values
+test_csv.to_csv('ova.csv', index=False)
---
+++ 15_classifier_ovo.py
@@ -0,0 +1,79 @@
+# Import libraries
+import pandas as pd       # Importing for panel data analysis
+import numpy as np
+import pickle             # Loading model
+import warnings           # Importing warning to disable runtime warnings
+warnings.filterwarnings("ignore") 
+
+''' Let's analyze the data in **penguins_test.csv** '''
+# Import training data into pandas dataframe
+test_csv = pd.read_csv('./penguins_test.csv')
+# print('Shape of the dataset:', test_csv.shape)
+# test_csv.head(3)
+
+# Let's drop the null values, as to make predictions using the trained SVM ono-vs-all and one-vs-one classifiers, we need the values of all the attributes.
+test_csv.dropna(inplace=True)
+
+X_encoded = test_csv.copy()
+
+''' Let's normalize the data (continuous attributes). We are choosing z-score normalization to retain the outlier information. '''
+X_encoded["Culmen Length (mm)"] = (X_encoded["Culmen Length (mm)"] - X_encoded["Culmen Length (mm)"].mean())/X_encoded["Culmen Length (mm)"].std()
+X_encoded["Culmen Depth (mm)"] = (X_encoded["Culmen Depth (mm)"] - X_encoded["Culmen Depth (mm)"].mean())/X_encoded["Culmen Depth (mm)"].std()
+X_encoded["Flipper Length (mm)"] = (X_encoded["Flipper Length (mm)"] - X_encoded["Flipper Length (mm)"].mean())/X_encoded["Flipper Length (mm)"].std()
+X_encoded["Body Mass (g)"] = (X_encoded["Body Mass (g)"] - X_encoded["Body Mass (g)"].mean())/X_encoded["Body Mass (g)"].std()
+X_encoded["Delta 15 N (o/oo)"] = (X_encoded["Delta 15 N (o/oo)"] - X_encoded["Delta 15 N (o/oo)"].mean())/X_encoded["Delta 15 N (o/oo)"].std()
+X_encoded["Delta 13 C (o/oo)"] = (X_encoded["Delta 13 C (o/oo)"] - X_encoded["Delta 13 C (o/oo)"].mean())/X_encoded["Delta 13 C (o/oo)"].std()
+
+# test_csv.head(3)
+
+''' Let's preprocess this **test_csv** the same way as we did for train_csv '''
+# Prepare data. This format must match the `X_train_ova` or `X_train_ovo` from `15_Assignment4.ipynb`
+data = X_encoded[['Island', 'Clutch Completion', 'Culmen Length (mm)', 'Culmen Depth (mm)', 'Flipper Length (mm)', 'Body Mass (g)', 'Sex', 'Delta 15 N (o/oo)', 'Delta 13 C (o/oo)']]
+X_encoded = pd.get_dummies(data, columns=['Island'])
+X_encoded.replace({'Yes': 1, 'No': 0}, inplace=True)
+X_encoded.replace({'FEMALE': 1, 'MALE': 0}, inplace=True)
+X_encoded.replace({True: 1, False: 0}, inplace=True)
+
+svm_classifiers_ovo = {} # Initialize a dictionary to store classifiers for each of the three binary class problems
+
+# load the model from disk
+loaded_model_adelie_gentoo = pickle.load(open('./ovo_svm_final_adelie_gentoo.sav', 'rb'))
+svm_classifiers_ovo[('Adelie Penguin (Pygoscelis adeliae)', 'Gentoo penguin (Pygoscelis papua)')] = loaded_model_adelie_gentoo
+loaded_model_adelie_chinstrap = pickle.load(open('./ovo_svm_final_adelie_chinstrap.sav', 'rb'))
+svm_classifiers_ovo[('Adelie Penguin (Pygoscelis adeliae)', 'Chinstrap penguin (Pygoscelis antarctica)')] = loaded_model_adelie_chinstrap
+loaded_model_gentoo_chinstrap = pickle.load(open('./ovo_svm_final_gentoo_chinstrap.sav', 'rb'))
+svm_classifiers_ovo[('Gentoo penguin (Pygoscelis papua)', 'Chinstrap penguin (Pygoscelis antarctica)')] = loaded_model_gentoo_chinstrap
+# print(svm_classifiers_ovo)
+# it should be of the form:
+# svm_classifiers_ovo  {('Adelie Penguin (Pygoscelis adeliae)', 'Gentoo penguin (Pygoscelis papua)'): SVC(C=0.005, kernel='linear', random_state=42), ('Adelie Penguin (Pygoscelis adeliae)', 'Chinstrap penguin (Pygoscelis antarctica)'): SVC(C=0.005, kernel='linear', random_state=42), ('Gentoo penguin (Pygoscelis papua)', 'Chinstrap penguin (Pygoscelis antarctica)'): SVC(C=0.005, kernel='linear', random_state=42)}
+
+''' Making predictions on Test set via trained binary classifiers '''
+predictions_ovo = [] # Initialize an empty list to store predictions
+
+# Iterate through each data point in the test set
+for index, row in X_encoded.iterrows():
+    # print("row:\n", row)
+    class_votes = {} # Initialize a dictionary to store class votes
+    
+    # Use each OvO classifier to vote for a class
+    for (class_1, class_2), svm_classifier in svm_classifiers_ovo.items():
+        y_pred = svm_classifier.predict([row])[0]
+    #     # print(f"For classes {class_1} & {class_2}")
+    #     # print("y_pred: ", y_pred)
+        if y_pred == 1:
+            class_votes[class_1] = class_votes.get(class_1, 0) + 1
+        else:
+            class_votes[class_2] = class_votes.get(class_2, 0) + 1
+    #     print("class_votes: ", class_votes)
+    
+    # Determine the predicted class with the most votes
+    predicted_class = max(class_votes, key=class_votes.get)
+#     # print("predicted_class of the row: ", predicted_class)
+
+    predictions_ovo.append(predicted_class)
+print("predictions for all Test rows: ", predictions_ovo)
+
+''' Save the data to a csv file called `ova.csv`'''
+# Appending the prediction results in the dataframe
+test_csv['predicted'] = np.array(predictions_ovo)
+test_csv.to_csv('ovo.csv', index=False)
